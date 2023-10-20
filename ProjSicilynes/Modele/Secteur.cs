3e4a003f255e1f244057969afdba61ff460e14fb
TraoreB8 <traorebdg@gmail.com>
2023-10-20 12:40:46 +0200

    Ajout de la classe Secteur

---
+++ ProjSicilynes/Modele/Secteur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjSicilynes.Modele
+{
+    internal class Secteur
+    {
+        private int id;
+        private string nom;
+
+        public Secteur(int unId, string unNom)
+        {
+            this.id = unId;
+            this.nom = unNom;
+        }
+        // Les nouveaux get et set version C# et VS.Net. Remplacent getLogin() et setLogin(string unLogin)
+        public string Nom { get => nom; set => nom = value; }
+
+        // remplace getId()
+        public int Id { get => id; }
+
+        // pour afficher la liste par la suite
+        public string Description
+        {
+            get => "Id : " + this.id + " Nom :" + this.nom;
+        }
+
+    }
+}
